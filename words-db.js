387b31fe8342294859fb41533eb763ac01bf9a28
frontDevGabs <9810gabriel@gmail.com>
2023-10-11 20:31:22 -0300

    first commit

---
+++ words-db.js
@@ -0,0 +1,198 @@
+let wordList = [
+    {
+        word: "banana",
+        hint: "fruta amarela"
+    },
+    {
+        word: "bala",
+        hint: "doce e açucarado"
+    },
+    {
+        word: "prata",
+        hint: "metal acinzentado"
+    },
+    {
+        word: "celular",
+        hint: "dispositivo eletronico"
+    },
+    {
+        word: "google",
+        hint: "famosa forma de pesquisa"
+    },
+    {
+        word: "dançar",
+        hint: "mover-se com a musica"
+    },
+    {
+        word: "limão",
+        hint: "fruta"
+    },
+    {
+        word: "coelho",
+        hint: "animal fofinho"
+    },
+    {
+        word: "aranha",
+        hint: "inseto com 8 pernas"
+    },
+    {
+        word: "tigre",
+        hint: "gato grande"
+    },
+    {
+        word: "tio",
+        hint: "irmão do meu pai é meu"
+    },
+    {
+        word: "virus",
+        hint: "doença"
+    },
+    {
+        word: "pão",
+        hint: "feito no forno"
+    },
+    {
+        word: "cerveja",
+        hint: "bebida alcoolica"
+    },
+    {
+        word: "espatula",
+        hint: "utencilio"
+    },
+    {
+        word: "hockey",
+        hint: "esporte no gelo"
+    },
+    {
+        word: "jaqueta",
+        hint: "um tipo de roupa"
+    },
+    {
+        word: "folha",
+        hint: "parte da arvore"
+    },
+    {
+        word: "laranja",
+        hint: "uma fruta"
+    },
+    {
+        word: "pepper",
+        hint: "spice"
+    },
+    {
+        word: "flauta",
+        hint: "um instrumento musical"
+    },
+    {
+        word: "nissan",
+        hint: "marca de carro"
+    },
+    {
+        word: "marte",
+        hint: "planeta do sistema solar"
+    },
+    {
+        word: "email",
+        hint: "relacionado a troca de mensagens"
+    },
+    {
+        word: "ideia",
+        hint: "uma sugestão"
+    },
+    {
+        word: "servidor",
+        hint: "relacionado a computadores ou sistemas"
+    },
+    {
+        word: "jpeg",
+        hint: "um formato de imagem"
+    },
+    {
+        word: "pesquisa",
+        hint: "ato de procurar algo"
+    },
+    {
+        word: "chave",
+        hint: "pequena peça de metal"
+    },
+    {
+        word: "egito",
+        hint: "nome de um país"
+    },
+    {
+        word: "holanda",
+        hint: "é um país da europa"
+    },
+    {
+        word: "fotografia",
+        hint: "representação de pessoa ou cenario"
+    },
+    {
+        word: "nilo",
+        hint: "maior rio do mundo"
+    },
+    {
+        word: "chuva",
+        hint: "relacionado a água"
+    },
+    {
+        word: "java",
+        hint: "linguagem de programação"
+    },
+    {
+        word: "guitarra",
+        hint: "instrumento musical"
+    },
+    {
+        word: "venus",
+        hint: "planeta do sistema solar"
+    },
+    {
+        word: "ouro",
+        hint: "um metal precioso"
+    },
+    {
+        word: "amazom",
+        hint: "site de compras online"
+    },
+    {
+        word: "python",
+        hint: "linguagem de programação"
+    },
+    {
+        word: "matrix",
+        hint: "filme de ficção cientifica"
+    },
+    {
+        word: "bugs",
+        hint: "relacionado a programação"
+    },
+    {
+        word: "png",
+        hint: "um formato de imagem"
+    },
+    {
+        word: "cerebro",
+        hint: "relacionado a cabeça"
+    },
+    {
+        word: "mapa",
+        hint: "diagrama que representa uma areá"
+    },
+    {
+        word: "ilha",
+        hint: "terreno rodeado por água"
+    },
+    {
+        word: "futebol",
+        hint: "um esporte famoso"
+    },
+    {
+        word: "xadrez",
+        hint: "relacionado a um jogo"
+    },
+    {
+        word: "instagram",
+        hint: "App de rede social"
+    },
+]
